f3ef6c2c1f4523a97757386df51e8fbfd2db99b0
apoowa <159249955+apoowa@users.noreply.github.com>
2024-11-01 08:36:56 +0000

    .

--- script.js
+++ script.js
@@ -16,4 +16,26 @@ button.addEventListener('click', function() {
     text.textContent = "text zmenen";
 });
 // ukol 4
+var showHide = document.getElementById("showHide");
+var demo = document.getElementById("demo");
+showHide.onclick = function() {
+  if (demo.style.display == "block") {
+    demo.style.display = "none";
+  } else {
+    demo.style.display = "block";
+  }
+}
+// ukol 5
+function sayHello() {
+    const name = document.getElementById('nameInput').value;
+    if(name) {
+        alert(`Ahoj, ${name}!`);
+    } else {
+        alert("Prosím, zadejte své jméno.");
+    }
+}
+
+
+
+// ukol 6
 
